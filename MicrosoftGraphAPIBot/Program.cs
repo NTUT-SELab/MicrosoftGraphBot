f69ab729ee6e1ac291f7964663668e64dfafb346
Kenneth Tang <kenneth@nssh.onmicrosoft.com>
2020-05-02 18:35:57 +0800

    加入專案檔案。

---
+++ MicrosoftGraphAPIBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MicrosoftGraphAPIBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
