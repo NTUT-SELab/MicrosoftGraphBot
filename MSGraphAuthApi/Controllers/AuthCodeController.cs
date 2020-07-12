c9487736278a305f09a9c10134be3f6a61f4de6f
Kenneth Tang <kenneth@nssh.onmicrosoft.com>
2020-07-13 01:45:47 +0800

    解析 Authorization code 的 web api https://msgraphauthorization.azurewebsites.net

---
+++ MSGraphAuthApi/Controllers/AuthCodeController.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MSGraphAuthApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AuthCodeController : ControllerBase
+    {
+        [HttpGet]
+#nullable enable
+        public string Get(string? code, string? error)
+#nullable disable
+        {
+            if (!string.IsNullOrWhiteSpace(code))
+                return JsonSerializer.Serialize(new { code });
+
+            if (!string.IsNullOrWhiteSpace(error))
+                return JsonSerializer.Serialize(new { error });
+
+            return "https://github.com/NTUT-SELab/MicrosoftGraphBot";
+        }
+    }
+}
---
+++ MicrosoftGraphBotTests/MSGraphAuthApiTests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MicrosoftGraphBotTests
+{
+    [TestClass]
+    public class MSGraphAuthApiTests
+    {
+        private readonly HttpClient httpClient;
+
+        public MSGraphAuthApiTests()
+        {
+            var webAppFixture = new WebApplicationFactory<MSGraphAuthApi.Startup>();
+            httpClient = webAppFixture.CreateClient();
+        }
+
+        [TestMethod]
+        public async Task TestGetAsync()
+        {
+            var buffer = await httpClient.GetAsync("/AuthCode");
+            string content = await buffer.Content.ReadAsStringAsync();
+
+            Assert.AreEqual("https://github.com/NTUT-SELab/MicrosoftGraphBot", content);
+        }
+
+        [TestMethod]
+        public async Task TestGetCodeAsync()
+        {
+            Guid id = Guid.NewGuid();
+            var buffer = await httpClient.GetAsync($"/AuthCode/?Code={id}");
+            string json = await buffer.Content.ReadAsStringAsync();
+            JObject jObject = JObject.Parse(json);
+
+            if (jObject.Property("code") == null)
+            {
+                Assert.Fail("Code parameter is null");
+            }
+
+            Assert.AreEqual(id.ToString(), jObject["code"]);
+        }
+
+        [TestMethod]
+        public async Task TestGetErrorAsync()
+        {
+            Guid id = Guid.NewGuid();
+            var buffer = await httpClient.GetAsync($"/AuthCode/?Error={id}");
+            string json = await buffer.Content.ReadAsStringAsync();
+            JObject jObject = JObject.Parse(json);
+
+            if (jObject.Property("error") == null)
+            {
+                Assert.Fail("Error parameter is null");
+            }
+
+            Assert.AreEqual(id.ToString(), jObject["error"]);
+        }
+    }
+}
