7f8dd42c1269a4ee62c19ec1ae38a8ec72e4cbd9
Kenneth Tang <kenneth@nssh.onmicrosoft.com>
2020-07-14 02:08:36 +0800

    修改 web api 根頁面內容

--- MSGraphAuthApi/Startup.cs
+++ MSGraphAuthApi/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -37,6 +38,11 @@ namespace MSGraphAuthApi
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("https://github.com/NTUT-SELab/MicrosoftGraphBot");
+                });
+
                 endpoints.MapControllers();
             });
         }
--- MicrosoftGraphBotTests/MSGraphAuthApiTests.cs
+++ MicrosoftGraphBotTests/MSGraphAuthApiTests.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSGraphAuthApi.Controllers;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Net.Http;
@@ -10,12 +11,70 @@ namespace MicrosoftGraphBotTests
     [TestClass]
     public class MSGraphAuthApiTests
     {
-        private readonly HttpClient httpClient;
+        private AuthCodeController authCodeController;
 
         public MSGraphAuthApiTests()
         {
+            authCodeController = new AuthCodeController();
+        }
+
+        [TestMethod]
+        public void TestGetAsync()
+        {
+            string content = authCodeController.Get(null, null);
+
+            Assert.AreEqual("https://github.com/NTUT-SELab/MicrosoftGraphBot", content);
+        }
+
+        [TestMethod]
+        public void TestGetCodeAsync()
+        {
+            Guid id = Guid.NewGuid();
+            string json = authCodeController.Get(id.ToString(), null);
+            JObject jObject = JObject.Parse(json);
+
+            if (jObject.Property("code") == null)
+            {
+                Assert.Fail("Code parameter is null");
+            }
+
+            Assert.AreEqual(id.ToString(), jObject["code"]);
+        }
+
+        [TestMethod]
+        public void TestGetErrorAsync()
+        {
+            Guid id = Guid.NewGuid();
+            string json = authCodeController.Get(null, id.ToString());
+            JObject jObject = JObject.Parse(json);
+
+            if (jObject.Property("error") == null)
+            {
+                Assert.Fail("Error parameter is null");
+            }
+
+            Assert.AreEqual(id.ToString(), jObject["error"]);
+        }
+    }
+
+    [TestClass]
+    public class MSGraphAuthApiUseRequestTests
+    {
+        private readonly HttpClient httpClient;
+
+        public MSGraphAuthApiUseRequestTests()
+        {
             var webAppFixture = new WebApplicationFactory<MSGraphAuthApi.Startup>();
             httpClient = webAppFixture.CreateClient();
+        }
+
+        [TestMethod]
+        public async Task TestRootAsync()
+        {
+            var buffer = await httpClient.GetAsync("/");
+            string content = await buffer.Content.ReadAsStringAsync();
+
+            Assert.AreEqual("https://github.com/NTUT-SELab/MicrosoftGraphBot", content);
         }
 
         [TestMethod]
